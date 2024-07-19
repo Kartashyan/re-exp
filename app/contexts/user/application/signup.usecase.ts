227181c2b0f8b0abb2a26a7e76ee82821f7d5b1c
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-19 13:37:18 +0200

    feat: Implement SignupUseCase for user signup functionality

---
+++ app/contexts/user/application/signup.usecase.ts
@@ -0,0 +1,45 @@
+
+import { fail, ok, Result } from "~/contexts/core/result";
+import { Email } from "../domain/email.value-object";
+import { UserRepository } from "../domain/user-repo.port";
+import { DomainError } from "~/contexts/core/domain-error";
+import { User } from "../domain/user.aggregate-root";
+import { Password } from "~/contexts/core/password.value-object";
+
+
+type CommandDto = {
+    email: string;
+    password: string;
+}
+
+export class SignupUseCase {
+    constructor(private readonly userRepo: UserRepository) { }
+
+    async execute(command: CommandDto): Promise<Result<true>> {
+        let email: Email;
+        let password: Password;
+        try {
+            email = Email.create(command.email);
+            password = Password.create(command.password);
+        } catch (e) {
+            const error = e as DomainError;
+            return fail(error.message);
+        }
+
+        const isExist = await this.userRepo.exists(email.value);
+
+        if (isExist) {
+            return fail('User already exists');
+        }
+        const hashedPassword = password.hash();
+        
+        const user = User.create({
+            email,
+            hashedPassword,
+        });
+
+        await this.userRepo.save(user);
+
+        return ok(true);
+    }
+}
