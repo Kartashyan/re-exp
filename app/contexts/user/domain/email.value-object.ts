6247e4e3371c95a2ab9342cf5dd13e5e907166b8
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-19 13:37:13 +0200

    feat: Add Email value object for user email validation

---
+++ app/contexts/user/domain/email.value-object.ts
@@ -0,0 +1,25 @@
+import { DomainError } from "~/contexts/core/domain-error";
+import { ValueObject } from "~/contexts/core/value-object";
+
+export class Email extends ValueObject<string> {
+    private constructor(props: string) {
+        super(props);
+    }
+
+    public static create(email: string): Email {
+        if (!Email.validate(email)) {
+            throw new DomainError('Invalid email');
+        }
+
+        return new Email(email);
+    }
+
+    private static validate(email: string): boolean {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    get value() {
+        return this.props;
+    }
+}
