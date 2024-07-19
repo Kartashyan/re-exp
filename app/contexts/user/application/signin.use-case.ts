80133b3013705ed83d6c9d5bdcf5bbf35af5dcc3
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-19 13:37:25 +0200

    feat: Add SigninUseCase for user sign-in functionality

---
+++ app/contexts/user/application/signin.use-case.ts
@@ -0,0 +1,31 @@
+import { fail, ok, Result } from "~/contexts/core/result";
+import { UserRepository } from "../domain/user-repo.port";
+import { User } from "../domain/user.aggregate-root";
+import { Email } from "../domain/email.value-object";
+import { DomainError } from "~/contexts/core/domain-error";
+import { Password } from "~/contexts/core/password.value-object";
+
+export class SigninUseCase {
+    private readonly userRepo: UserRepository;
+    constructor(userRepo: UserRepository){
+        this.userRepo = userRepo;
+    }
+    async execute(email: string, password: string): Promise<Result<User>> {
+        try{
+            const user = await this.userRepo.findByEmail(Email.create(email).value);
+            if (!user) {
+                return fail("[signin.use-case]: User not found");
+            }
+            if (!user.comparePassword(Password.create(password))) {
+                return fail("[signin.use-case]: Invalid email or password");
+            }
+
+            return ok(user);
+        } catch (e) {
+            if (e instanceof DomainError) {
+                return fail(("[signin.use-case]: Invalid email or password"));
+            }
+            return fail("[signin.use-case]: Internal server error");
+        }
+    }
+}
