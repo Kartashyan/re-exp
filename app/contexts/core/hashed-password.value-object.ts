53763c282b670a5123d35c81a7673380a64e28e6
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-19 13:38:22 +0200

    feat: Add HashedPassword value object for managing hashed passwords

---
+++ app/contexts/core/hashed-password.value-object.ts
@@ -0,0 +1,32 @@
+import { DomainError } from "./domain-error";
+import { PasswordService } from "./password.service";
+import { Password } from "./password.value-object";
+import { ValueObject } from "./value-object";
+
+
+export class HashedPassword extends ValueObject<string> {
+    private passwordService: PasswordService;
+    constructor(value: string) {
+        super(value);
+        this.passwordService = new PasswordService();
+        if (!this.isValid()) {
+            throw new DomainError('Invalid hashed password');
+        }
+    }
+
+    get value(): string {
+        return this.props;
+    }
+
+    public compare(password: Password): boolean {
+        return this.passwordService.compare(password, this.props);
+    }
+
+    isValid(): boolean {
+        const hashLength = 60; // bcrypt hash length
+        if (this.props.length !== hashLength) {
+            return false;
+        }
+        return true;
+    }
+}
