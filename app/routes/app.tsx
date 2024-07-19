f860fb0333769c554d55291d27b7478ed8a7105a
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-19 13:29:44 +0200

    chore: Add app route component

---
+++ app/routes/app.tsx
@@ -0,0 +1,16 @@
+import { LoaderFunction } from "@remix-run/node";
+import { authenticator } from "~/auth/authenticator.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+    await authenticator.isAuthenticated(request, {
+        failureRedirect: "/login",
+    });
+}
+
+export default function AppPage() {
+    return (
+        <div>
+            <h1>App</h1>
+        </div>
+    );
+}
