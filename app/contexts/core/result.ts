a5da1ef5ecd8398b0badf7d1a9281a556e62b94f
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-19 13:37:46 +0200

    feat: Add Result type for representing success or failure outcomes

---
+++ app/contexts/core/result.ts
@@ -0,0 +1,13 @@
+type Success<T> = {
+    success: true;
+    value: T;
+};
+type Failure<E = string> = {
+    success: false;
+    reason: E;
+};
+
+export type Result<T, E = string> = Success<T> | Failure<E>;
+
+export const ok = <T>(value: T): Success<T> => ({ success: true, value });
+export const fail = <E = string>(reason: E): Failure<E> => ({ success: false, reason});
