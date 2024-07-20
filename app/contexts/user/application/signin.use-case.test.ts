acd01fc0eb0b333ec3eb4853303cb6c07cbf31d0
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-20 16:21:19 +0200

    chore: Add user signin and signup use-case tests

---
+++ app/contexts/user/application/signin.use-case.test.ts
@@ -0,0 +1,76 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { SigninUseCase } from "./signin.use-case";
+import { UserRepository } from "../domain/user-repo.port";
+import { Password } from "~/contexts/core/password.value-object";
+import { User } from "../domain/user.aggregate-root";
+import { Email } from "../domain/email.value-object";
+import { fail, ok } from "~/contexts/core/result";
+
+describe("SigninUseCase", () => {
+    let userRepo: UserRepository;
+    let signinUseCase: SigninUseCase;
+
+    beforeEach(() => {
+        userRepo = {
+            findByEmail: vi.fn(),
+            save: vi.fn(),
+            exists: vi.fn(),
+        };
+        signinUseCase = new SigninUseCase(userRepo);
+    });
+
+    afterEach(() => {
+        vi.clearAllMocks();
+    });
+
+    it("should return a user model when valid email and password are provided", async () => {
+        const email = "test@example.com";
+        const password = "password";
+        const hashedPassword = Password.create(password).hash();
+        const user = User.create({ email: Email.create(email), hashedPassword });
+        
+        vi.spyOn(userRepo, "findByEmail").mockResolvedValue(user);
+        user.comparePassword = vi.fn().mockReturnValue(true);
+        
+        const result = await signinUseCase.execute(email, password);
+        
+        expect(userRepo.findByEmail).toHaveBeenCalledWith(email);
+        expect(user.comparePassword).toHaveBeenCalledWith(expect.any(Password));
+        expect(result).toEqual(ok(user));
+    });
+
+    it("should return a failure result when user is not found", async () => {
+        const email = "test@example.com";
+        const password = "password";
+
+        vi.spyOn(userRepo, "findByEmail").mockResolvedValue(null);
+
+        const result = await signinUseCase.execute(email, password);
+
+        expect(userRepo.findByEmail).toHaveBeenCalledWith(email);
+        expect(result).toEqual(fail("[signin.use-case]: User not found"));
+    });
+
+    it("should return a failure result when invalid email or password is provided", async () => {
+        const email = "invalid";
+        const password = "inv";
+
+        const result = await signinUseCase.execute(email, password);
+
+        expect(result).toEqual(fail("[signin.use-case]: Invalid email or password"));
+    });
+
+    it("should return a failure result when an error occurs", async () => {
+        const email = "test@example.com";
+        const password = "password";
+
+        vi.spyOn(userRepo, "findByEmail").mockRejectedValue(new Error());
+
+        const result = await signinUseCase.execute(email, password);
+
+        expect(userRepo.findByEmail).toHaveBeenCalledWith(email);
+
+        expect(result).toEqual(fail("[signin.use-case]: Internal server error"));
+    });
+});
+
---
+++ app/contexts/user/application/signup.usecase.test.ts
@@ -0,0 +1,83 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { UserRepository } from "../domain/user-repo.port";
+import { SignupUseCase } from "./signup.usecase";
+import { fail, ok } from "~/contexts/core/result";
+
+describe("SignupUseCase", () => {
+  let userRepo: UserRepository;
+  let signupUseCase: SignupUseCase;
+
+  beforeEach(() => {
+    userRepo = {
+      exists: vi.fn(),
+      save: vi.fn(),
+      findByEmail: vi.fn(),
+    };
+    signupUseCase = new SignupUseCase(userRepo);
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("should create a new user when valid email and password are provided", async () => {
+    // Arrange
+    const command = {
+      email: "test@example.com",
+      password: "password123",
+    };
+
+    vi.spyOn(userRepo, "exists").mockResolvedValue(false);
+
+    // Act
+    const result = await signupUseCase.execute(command);
+
+    // Assert
+    expect(result).toEqual(ok(true));
+  });
+
+  it("should return an error when email or password is invalid", async () => {
+    // Arrange
+    const command = {
+      email: "invalid-email",
+      password: "short",
+    };
+
+    // Act
+    const result = await signupUseCase.execute(command);
+
+    // Assert
+    expect(result).toEqual(fail("Invalid email"));
+  });
+
+  it("should return an error when email or password is invalid", async () => {
+    // Arrange
+    const command = {
+      email: "valid@email.com",
+      password: "short",
+    };
+
+    // Act
+    const result = await signupUseCase.execute(command);
+
+    // Assert
+    expect(result).toEqual(fail("Invalid password"));
+  });
+
+  it("should return an error when user already exists", async () => {
+    // Arrange
+    const command = {
+      email: "existing-user@example.com",
+      password: "password123",
+    };
+
+    vi.spyOn(userRepo, "exists").mockResolvedValue(true);
+
+    // Act
+    const result = await signupUseCase.execute(command);
+
+    // Assert
+    expect(userRepo.exists).toHaveBeenCalledWith(command.email);
+    expect(result).toEqual(fail("User already exists"));
+  });
+});
