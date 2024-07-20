3629e9e14aae67500d53ba3edba45af15ad35cff
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-20 12:50:27 +0200

    chore: Remove debugger statements from password service and signup usecase

--- app/contexts/core/password.service.server.ts
+++ app/contexts/core/password.service.server.ts
@@ -3,7 +3,6 @@ import { Password } from './password.value-object';
 
 export class PasswordService {
     public hash(password: Password): string {
-        debugger;
         const salt = bcrypt.genSaltSync(10);
         return bcrypt.hashSync(password.value, salt);
     }
