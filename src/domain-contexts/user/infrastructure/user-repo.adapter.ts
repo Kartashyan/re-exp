e861920bde17ea9fb3a963fac05f485c78b522b6
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-21 17:26:37 +0200

    feat: Implement UserDrizzleRepositoryAdapter for user repository

--- src/domain-contexts/user/infrastructure/user-repo.adapter.ts
+++ src/domain-contexts/user/infrastructure/user-repo.adapter.ts
@@ -1,3 +1,9 @@
+import { eq } from 'drizzle-orm';
+import { HashedPassword } from "~/domain-contexts/core/hashed-password.value-object";
+import { UID } from "~/domain-contexts/core/id";
+import { db } from "~/infrastructure/storage/db";
+import { usersTable } from "~/infrastructure/storage/schema";
+import { Email } from "../domain/email.value-object";
 import { UserRepository } from "../domain/user-repo.port";
 import { User } from "../domain/user.aggregate-root";
 
@@ -15,5 +21,29 @@ export class UserLocalMemoryRepositoryAdapter implements UserRepository {
 
     async exists(email: string): Promise<boolean> {
         return !!this.users.find((user) => user.email.value === email);
+    }
+}
+
+export class UserDrizzleRepositoryAdapter implements UserRepository {
+    private db = db;
+    async save(user: User): Promise<void> {
+        await this.db.insert(usersTable).values({
+            id: user.id.value,
+            email: user.email.value,
+            password: user.password.value,
+        })
+    }
+    async findByEmail(email: string): Promise<User | null> {
+        const users = await this.db.select().from(usersTable).where(eq(usersTable.email, email));
+        if (!users.length) return null;
+        const user = users[0];
+        return User.create({
+            email: Email.create(user.email),
+            hashedPassword: new HashedPassword(user.password),
+        }, new UID(user.id));
+    }
+    async exists(email: string): Promise<boolean> {
+        const users = await this.db.select().from(usersTable).where(eq(usersTable.email, email));
+        return !!users.length
     }
 }
